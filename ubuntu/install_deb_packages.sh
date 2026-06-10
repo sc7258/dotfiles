0fe1f9ee68e6720257feed42da81209568fa0028
sungchul.chin <sungchul.shin@intelliantech.com>
2026-06-10 17:06:31 +0900

    Update Synology Drive Client download URL to 4.0.3-17892

--- ubuntu/install_deb_packages.sh
+++ ubuntu/install_deb_packages.sh
@@ -21,7 +21,7 @@ fi
 
 # 3. Synology Drive Client (정적 링크 활용 - URL이 변경될 수 있음)
 echo "  -> Synology Drive Client 다운로드 및 설치..."
-SYNOLOGY_URL="https://global.download.synology.com/download/Utility/SynologyDriveClient/3.4.0-15724/Ubuntu/Installer/synology-drive-client-10000.x86_64.deb"
+SYNOLOGY_URL="https://global.synologydownload.com/download/Utility/SynologyDriveClient/4.0.3-17892/Ubuntu/Installer/synology-drive-client-17892.x86_64.deb"
 wget -qO synology-drive.deb "$SYNOLOGY_URL"
 sudo apt install -y ./synology-drive.deb
 
